8f0ca816a4e3fa84acb8bdc217a88be1ee6e54e0
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-06-21 20:43:56 +0300

    feat: Exercise 2 completed

--- Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-2/Card.swift
@@ -4,7 +4,7 @@ protocol CardBaseCompatible: Hashable, Codable {
     var suit: Suit {get}
     var value: Value {get}
     var isTrump: Bool {get}
-
+    
     func hash(into hasher: inout Hasher)
 }
 
@@ -31,22 +31,34 @@ struct Card: CardBaseCompatible {
     let suit: Suit
     let value: Value
     var isTrump: Bool = false
-
+    
     func hash(into hasher: inout Hasher) {
-
+        
     }
-
+    
     static func == (lhs: Self, rhs: Self) -> Bool {
-        return false
+        if lhs.value == rhs.value && lhs.suit == rhs.suit && lhs.isTrump == rhs.isTrump {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
 extension Card {
-
+    
     func checkIfCanBeat(card: Card) -> Bool {
-        return false
+        let canBeat: Bool
+        if self.value.rawValue > card.value.rawValue && self.suit == card.suit {
+            canBeat = true
+        } else if self.isTrump && !card.isTrump {
+            canBeat = true
+        } else {
+            canBeat = false
+        }
+        return canBeat
     }
-
+    
     func checkValue(card: Card) -> Bool {
         return false
     }
